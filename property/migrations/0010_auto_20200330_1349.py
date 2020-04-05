ff385c5ceb76ed953c9b4f5b3a97a36e09a00ae3
IvanPolienko <piar1986@yandex.ru>
2020-04-05 14:25:52 +0700

    Corrected search options

--- property/migrations/0010_auto_20200330_1349.py
+++ property/migrations/0010_auto_20200330_1349.py
@@ -8,9 +8,9 @@ from property.models import Owner
 def load_owners(apps, schema_editor):
     Owner = apps.get_model('property','Owner')
     for flat in Flat.objects.all():
-        Owner.objects.get_or_create(name=flat.owner, defaults={
+        Owner.objects.get_or_create(phone_pure=flat.owner_phone_pure, defaults={
+            'name': flat.owner,
             'phonenumber': flat.owners_phonenumber,
-            'phone_pure': flat.owner_phone_pure,
         })
 
 class Migration(migrations.Migration):
